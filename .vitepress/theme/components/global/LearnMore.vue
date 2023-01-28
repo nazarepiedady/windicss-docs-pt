562cf8ff70d7d79f607f5c4fcf9b81e820f5f42e
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-01-28 06:17:42 +0000

    docs(pt): translate `.vitepress/theme/components/global/learnmore.vue`

--- .vitepress/theme/components/global/LearnMore.vue
+++ .vitepress/theme/components/global/LearnMore.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <div class="mt-4">
     <a :href="to" class="text-primary border-b border-transparent !hover:(no-underline border-current)">
-      Learn more <carbon:arrow-right class="inline" />
+      Aprenda Mais <carbon:arrow-right class="inline" />
     </a>
   </div>
 </template>
