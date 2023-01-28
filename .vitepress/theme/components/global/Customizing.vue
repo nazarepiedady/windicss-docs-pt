68ac7b8fc60a241ae8910bd951821bfd530894d6
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-01-28 06:18:20 +0000

    docs(pt): translate `.vitepress/theme/components/global/customizing.vue`

--- .vitepress/theme/components/global/Customizing.vue
+++ .vitepress/theme/components/global/Customizing.vue
@@ -1,7 +1,7 @@
 <template>
   <details class="customizing mt-4">
     <summary class="outline-none select-none cursor-pointer opacity-50">
-      Customizing
+      Personalizando
     </summary>
     <div class="mt-4">
       <slot />
